db5d656df6ce1e99459b03d850736707864bd0d7
Arpan Abhishek <arpanforbusiness@gmail.com>
2019-06-19 01:47:18 +0530

    Updates

---
+++ precache-manifest.d2ae96689bf6ab960acf5ff4b2224600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707e9b46c22bcfe0f7fcbdaa909200b5",
+    "url": "/knowme/index.html"
+  },
+  {
+    "revision": "d0f8a128a2e5c3cfa415",
+    "url": "/knowme/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/knowme/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d0f8a128a2e5c3cfa415",
+    "url": "/knowme/static/js/main.8793c63a.chunk.js"
+  },
+  {
+    "revision": "f573838bc4512a2884fc",
+    "url": "/knowme/static/js/runtime~main.2d8392f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/knowme/static/media/logo.5d5d9eef.svg"
+  }
+]);
